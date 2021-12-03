f7ade73edf45682911df8d64d4ca240ce4140089
1806694738 <86834383+1806694738@users.noreply.github.com>
2021-12-03 21:21:10 +0800

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,19 +4,19 @@
 
 text _stdcall MQ_Info () {
 	json j;
-	j.Add ("name", "墨墨商城");
-	j.Add ("author", "小墨");
-	j.Add ("description", "插件说明");
+	j.Add ("name", "澧ㄥⅷ鍟嗗煄");
+	j.Add ("author", "灏忓ⅷ");
+	j.Add ("description", "鎻掍欢璇存槑");
 	j.Add ("version", "1.0.0");
 	j.Add ("skey", "SDG5D4Ys89h7DJ849d");
 	j.Add ("sdk", "S1");
 	string str = j.ToString ();
 
-	//text j = "{ \"name\":\"墨墨商城\", \"author\" : \"墨\", \"description\" : \"插件说明\", \"version\" : \"1.0.0\", \"skey\" : \"SDG5D4Ys89h7DJ849d\", \"sdk\" : \"S1\"}";
+	//text j = "{ \"name\":\"澧ㄥⅷ鍟嗗煄\", \"author\" : \"澧╘", \"description\" : \"鎻掍欢璇存槑\", \"version\" : \"1.0.0\", \"skey\" : \"SDG5D4Ys89h7DJ849d\", \"sdk\" : \"S1\"}";
 	return str.c_str();
 }
 
-int  _stdcall MQ_Event (text a, int b,int c, text d, text e, text f, text g, text h, text i, text j, int k) {
+int _stdcall MQ_Event (text BotQQ, int Type,int Type1, text Group, text AdminQQ, text QQ, text Msg, text ID, text No, text Msgna, int h ){
 
 
 	return 1;
